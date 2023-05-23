9ddb4d44a227ee5cc12fd935e98d3b23cb24ce98
guan-kevin <20823498+guan-kevin@users.noreply.github.com>
2023-05-23 11:22:25 -0700

    Job will fail

--- workflows/basic.py
+++ workflows/basic.py
@@ -2,7 +2,7 @@ import yaml
 
 
 def get_workflow_name(workflow_file):
-    return yaml.safe_load(workflow_file)['name']
+    return yaml.safe_load(workflow_file)['jobs']
 
 def get_jobs_count(workflow_file):
     return len(yaml.safe_load(workflow_file)['jobs'])
