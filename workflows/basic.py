c627d559b3f1743f08ac34737da81e62e5dec0f1
guan-kevin <20823498+guan-kevin@users.noreply.github.com>
2023-05-23 11:22:39 -0700

    Job will pass

--- workflows/basic.py
+++ workflows/basic.py
@@ -2,7 +2,7 @@ import yaml
 
 
 def get_workflow_name(workflow_file):
-    return yaml.safe_load(workflow_file)['jobs']
+    return yaml.safe_load(workflow_file)['name']
 
 def get_jobs_count(workflow_file):
     return len(yaml.safe_load(workflow_file)['jobs'])
